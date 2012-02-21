36ef12aa9aa9c0ecc8c9079ca095d1c49f45f6f5
David Batey <davidlbatey@gmail.com>
2012-02-21 23:25:12 +0000

    Added url and locates the pathname

--- routing/routes.js
+++ routing/routes.js
@@ -1,4 +1,10 @@
+var url = require("url");
+
 function route(request, response){
+  
+  var pathname = url.parse(request.url).pathname;
+  console.log("New request for " + pathname)
+
   response.writeHead(200, {"Content-Type":"text/plain"});
   response.write("Woosh!");
   response.end();
